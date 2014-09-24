d0855793b0e1cbd38fc271850a958d0ca693e9d1
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-23 18:37:43 -0700

    Added root view controller

--- Blocstagram/Blocstagram/BLCAppDelegate.m
+++ Blocstagram/Blocstagram/BLCAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "BLCAppDelegate.h"
+#import "BLCImagesTableViewController.h"
 
 @implementation BLCAppDelegate
 
@@ -14,6 +15,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[BLCImagesTableViewController alloc] init]];
+    
+                                      
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Blocstagram/Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Anthony Dagati on 9/23/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+
+@end
