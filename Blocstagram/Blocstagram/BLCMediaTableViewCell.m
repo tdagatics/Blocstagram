6882519382ace8ecf36493eabafd459bf01a8612
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-25 14:13:25 -0700

    Third Complex Data Cells commit

--- Blocstagram/Blocstagram/BLCMediaTableViewCell.m
+++ Blocstagram/Blocstagram/BLCMediaTableViewCell.m
@@ -97,12 +97,29 @@ static NSParagraphStyle *paragraphStyle;
     return commentString;
 }
 
+-(CGSize) sizeOfString:(NSAttributedString *)string {
+    CGSize maxSize  = CGSizeMake(CGRectGetWidth(self.contentView.bounds) - 40, 0.0);
+    CGRect sizeRect = [string boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    sizeRect.size.height += 20;
+    sizeRect = CGRectIntegral(sizeRect);
+    return sizeRect.size;
+}
 
+-(void)layoutSubviews {
+    [super layoutSubviews];
+    
+    CGFloat imageHeight = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
+    self.mediaImageView.frame = CGRectMake(0, 0, CGRectGetWidth(self.contentView.bounds), imageHeight);
+    
+    CGSize sizeOfUsernameAndCaptionLabel = [self sizeOfString:self.usernameAndCaptionLabel.attributedText];
+    self.usernameAndCaptionLabel.frame = CGRectMake(0, CGRectGetMaxX(self.mediaImageView.frame), CGRectGetWidth(self.contentView.bounds), sizeOfUsernameAndCaptionLabel.height);
+    
+    CGSize sizeOfCommentLabel = [self sizeOfString:self.commentLabel.attributedText];
+    self.commentLabel.frame = CGRectMake(0, CGRectGetMaxY(self.usernameAndCaptionLabel.frame), CGRectGetWidth(self.bounds), sizeOfCommentLabel.height);
+    
 
-
-
-
-
-
+    // Hide the line between cells
+    self.separatorInset = UIEdgeInsetsMake(0, 0, 0, CGRectGetWidth(self.bounds));
+}
 
 @end
