fa9a9387600dbfe2e98d2661691b31cf49928673
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-25 13:32:11 -0700

    Initial Complex Data Cells commit

---
+++ Blocstagram/Blocstagram/BLCMediaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BLCMediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Anthony Dagati on 9/25/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCMediaTableViewCell : UITableViewCell
+
+@end
---
+++ Blocstagram/Blocstagram/BLCMediaTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  BLCMediaTableViewCell.m
+//  Blocstagram
+//
+//  Created by Anthony Dagati on 9/25/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import "BLCMediaTableViewCell.h"
+
+@implementation BLCMediaTableViewCell
+
+@end
